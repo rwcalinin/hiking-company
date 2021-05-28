85baf44df85aeb2f264feb22184d4f80d7a0f7ff
rwcalinin <rwcalinin@gmail.com>
2021-05-28 23:35:36 +0300

    packaje.json fix

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,4 @@ import slideSlider from './modules/slider';
 window.addEventListener('DOMContentLoaded', () => {
 
     slideSlider('.choose__left-arrow', '.choose__right-arrow', '.choose__gallery-inner', '.choose__gallery-inner img', '.choose__current-slide', '.choose__total-slide')
-
 });
