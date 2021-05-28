8f4d89091173131916a8fa28779b226c203a9a34
rwcalinin <rwcalinin@gmail.com>
2021-05-28 22:45:23 +0300

    slide counters

--- dist/js/script.js
+++ dist/js/script.js
@@ -104,19 +104,35 @@ function slideSlider(leftArrowSel, rightArrowSel, gallerySel, gallerySlidesSel, 
         totalCountSelector = document.querySelector(totalCountSel),
         // ? gallery selector:
   gallery = document.querySelector(gallerySel),
-        gallerySlides = document.querySelectorAll(gallerySlidesSel); // -311px at one slide
+        gallerySlides = document.querySelectorAll(gallerySlidesSel);
+  let count = 1,
+      sliderTransformX = 0; // -311px at one slide
 
-  gallery.style.transform = 'translateX(0px)';
+  gallery.style.transform = 'translateX(0px)'; // setTimeout(() => {
+  //     gallery.classList.toggle('hide');
+  // }, 1000);
+
   totalSlidesCountPaste(gallerySlides);
 
   function totalSlidesCountPaste(gall) {
     const totalSlides = gall.length;
-    console.log(totalSlides);
 
     if (totalSlides < 10) {
       totalCountSelector.textContent = `/0${totalSlides}`;
     } else {
       totalCountSelector.textContent = `/${totalSlides}`;
+    }
+  }
+
+  countCurrentSlide(currentCountSelector, gallerySlides);
+
+  function countCurrentSlide(curSel, gall) {
+    const totalSlides = gall.length;
+
+    if (totalSlides < 10) {
+      curSel.textContent = `0${sliderTransformX / -311 + 1}`;
+    } else {
+      curSel.textContent = `${sliderTransformX / -311 + 1}`;
     }
   }
 }
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -9,19 +9,35 @@ function slideSlider(leftArrowSel, rightArrowSel, gallerySel, gallerySlidesSel, 
           // ? gallery selector:
           gallery = document.querySelector(gallerySel),
           gallerySlides = document.querySelectorAll(gallerySlidesSel);
+    
+    let count = 1,
+        sliderTransformX = 0;
 
           // -311px at one slide
     gallery.style.transform = 'translateX(0px)';
+    // setTimeout(() => {
+    //     gallery.classList.toggle('hide');
+    // }, 1000);
 
     totalSlidesCountPaste(gallerySlides);
     function totalSlidesCountPaste(gall) {
         const totalSlides = gall.length;
-        console.log(totalSlides);
 
         if (totalSlides < 10) {
             totalCountSelector.textContent = `/0${totalSlides}`;
         } else {
             totalCountSelector.textContent = `/${totalSlides}`;
+        }
+    }
+
+    countCurrentSlide(currentCountSelector, gallerySlides);
+    function countCurrentSlide(curSel, gall) {
+        const totalSlides = gall.length;
+
+        if (totalSlides < 10) {
+            curSel.textContent = `0${(sliderTransformX / -311) + 1}`
+        } else {
+            curSel.textContent = `${(sliderTransformX / -311) + 1}`
         }
     }
 
