ac9875f71509fdadca108428465ef943b9348228
rwcalinin <rwcalinin@gmail.com>
2021-05-28 21:33:25 +0300

    js ready

---
+++ src/js/modules/slider.js
@@ -0,0 +1,28 @@
+function slideSlider(leftArrowSel, rightArrowSel, gallerySel, gallerySlidesSel, currentCountSel, totalCountSel) {
+
+    const // ? arorws:
+          leftArrow = document.querySelector(leftArrowSel),
+          rightArrow = document.querySelector(rightArrowSel),
+          // ? counter:
+          currentCountSelector = document.querySelector(currentCountSel),
+          totalCountSelector = document.querySelector(totalCountSel),
+          // ? gallery selector:
+          gallery = document.querySelector(gallerySel),
+          gallerySlides = document.querySelectorAll(gallerySlidesSel);
+
+    totalSlidesCountPaste(gallerySlides);
+    function totalSlidesCountPaste(gall) {
+        const totalSlides = gall.length;
+        console.log(totalSlides);
+
+        if (totalSlides < 10) {
+            totalCountSelector.textContent = `/0${totalSlides}`;
+        } else {
+            totalCountSelector.textContent = `/${totalSlides}`;
+        }
+    }
+
+
+}
+
+export default slideSlider;
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log('hi');
+'use strict';
+
+import slideSlider from './modules/slider';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    slideSlider('.choose__left-arrow', '.choose__right-arrow', '.choose__gallery-inner', '.choose__gallery-inner img', '.choose__current-slide', '.choose__total-slide')
+
+});
